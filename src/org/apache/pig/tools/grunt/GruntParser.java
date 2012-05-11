e1180eff0880279dece8d1d498e758bef1034409
Jianyong Dai <daijy@apache.org>
2012-05-11 00:31:41 +0000

    PIG-2583: Add Grunt command to list the statements in cache
    git-svn-id: https://svn.apache.org/repos/asf/pig/trunk@1336978 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/pig/tools/grunt/GruntParser.java
+++ src/org/apache/pig/tools/grunt/GruntParser.java
@@ -812,7 +812,12 @@ public class GruntParser extends PigScriptParser {
             log.warn("'pwd' statement is ignored while processing 'explain -script' or '-check'");
         }
     }
-
+    
+    @Override
+	protected void processHistory(boolean withNumbers) {
+    	mPigServer.printHistory(withNumbers);
+    }
+    
     @Override
     protected void printHelp() 
     {
@@ -854,6 +859,8 @@ public class GruntParser extends PigScriptParser {
         System.out.println("        stream.skippath - String that contains the path. This is used by streaming.");
         System.out.println("        any hadoop property.");
         System.out.println("    help - Display this message.");
+        System.out.println("    history [-n] - Display the list statements in cache.");
+        System.out.println("        -n Hide line numbers. ");
         System.out.println("    quit - Quit the grunt shell.");
     }
 
@@ -1180,4 +1187,5 @@ public class GruntParser extends PigScriptParser {
     private int mNumSucceededJobs;
     private FsShell shell;
     private boolean mScriptIllustrate;
+    
 }
