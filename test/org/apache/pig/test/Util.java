57b1060949de5960dd37c6423a5655785668b225
cheolsoo <cheolsoo@cloudera.com>
2012-11-20 18:25:55 -0800

    CDH-4845 Fix CDH4 Pig unit test blowout

--- test/org/apache/pig/test/Util.java
+++ test/org/apache/pig/test/Util.java
@@ -571,9 +571,16 @@ public class Util {
 	 */
 	static public void copyFromLocalToCluster(MiniCluster cluster, String localFileName, String fileNameOnCluster) throws IOException {
         PigServer ps = new PigServer(ExecType.MAPREDUCE, cluster.getProperties());
-        String script = getMkDirCommandForHadoop2_0(fileNameOnCluster) + "fs -put " + localFileName + " " + fileNameOnCluster;
+        String script = "fs -put " + localFileName + " " + fileNameOnCluster;
 
-	    GruntParser parser = new GruntParser(new StringReader(script));
+        FileSystem fs = cluster.getFileSystem();
+        Path clusterFile = new Path(fileNameOnCluster);
+        Path clusterFileParent = clusterFile.getParent();
+        if (!fs.exists(clusterFileParent)) {
+            fs.mkdirs(clusterFileParent);
+        }
+
+        GruntParser parser = new GruntParser(new StringReader(script));
         parser.setInteractive(false);
         parser.setParams(ps);
         try {
