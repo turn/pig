828ff53f62f975ead01f5882e0bcebd08c162c32
Mohit Sabharwal <mohit@cloudera.com>
2014-11-21 15:44:42 -0800

    CLOUDERA-BUILD : CDH-23366 : PIG ILLUSTRATE command broken
    getScriptHash() needs logical plan set in the PigContext's execution engine,
    which is not true for ILLUSTRATE.
    
    In fact, in ILLUSTRATE's case, the logical plan is never set for PigContext
    execution engine. I'm not entirely sure why, but there is bunch of
    "if(!pigContext.inIllustrator)" code in Pig, so ILLUSTRATE as a special case is
    apparently pretty common :(
    
    This is related to a CDH-only Navigator change, so I can't really compare with
    upstream -- where much of the related code has been refactored anyways.
    
    (cherry picked from commit 384818400b500bbc58a0121cf1cb10265628e88b)

--- src/org/apache/pig/tools/pigstats/ScriptState.java
+++ src/org/apache/pig/tools/pigstats/ScriptState.java
@@ -297,8 +297,12 @@ public class ScriptState {
         conf.set(PIG_PROPERTY.SCRIPT.toString(), getScript());        
         conf.set(PIG_PROPERTY.COMMAND_LINE.toString(), getCommandLine());
         if (isPlanSerializationEnabled()) {
-          conf.set(PIG_PROPERTY.LOGICAL_PLAN.toString(), getSerializedLogicalPlan());
-          conf.set(PIG_PROPERTY.LOGICAL_PLAN_HASH.toString(), getScriptHash());
+          // CDH-23366 : In ILLUSTRATE command codepath, the PigContext's execution engine
+          // does not have the Logical Plan set at this point.
+          if (!pigContext.inIllustrator) {
+            conf.set(PIG_PROPERTY.LOGICAL_PLAN.toString(), getSerializedLogicalPlan());
+            conf.set(PIG_PROPERTY.LOGICAL_PLAN_HASH.toString(), getScriptHash());
+          }
         }
 
         try {
