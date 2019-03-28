aae25b359d3b0a17b52db88cf54b9f2df02abe2c
Sandy Sim <sandy.sim@turn.com>
2019-03-28 15:25:56 -0700

    Guava 19 version of cdh5-0.12.0_5.16.1
    Jira: TI-2618
    
    Signed-off-by: Sandy Sim <sandy.sim@turn.com>

--- docker/build.sh
+++ docker/build.sh
@@ -4,8 +4,12 @@
 # MAVEN_HOME for Maven 1 - M2_HOME for Maven 2
 export M2_HOME=/usr/local/apache-maven
 export PATH=${M2_HOME}/bin:${PATH}
+REPO_MVN="http://repo2.maven.org/maven2"
+REPO_ROOT="https://repository.cloudera.com/artifactory/cloudera-repos/"
+MVN_CMD="mvn --settings /home/jenkins/.m2/settings.xml -Dmvnrepo=\"$REPO_MVN\" -Drepository.root=\"$REPO-ROOT\" -f amobee-pom.xml"
+echo "172.19.112.132 maven.turn.com" >> /etc/hosts
 
 cd /pig
-mvn --settings /home/jenkins/.m2/settings.xml -f amobee-pom.xml clean deploy
-mvn --settings /home/jenkins/.m2/settings.xml -f amobee-pom.xml clean 
+$MVN_CMD clean deploy 
+$MVN_CMD clean 
 
